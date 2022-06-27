c5ded776f443ee9e2aea485e63c4a509de02d46d
abdoul20 <abdoulkarimfoly1996@gmail.com>
2022-06-27 11:44:18 +0000

    ce projet est une mise en oeuvre de notre application pour la certification en developpement web et web mobile a simplon senegal. Premier commit et initialisation du projet certification backend

---
+++ src/main/java/cours/java/springboot/certificationbackend/CertificationbackendApplication.java
@@ -0,0 +1,13 @@
+package cours.java.springboot.certificationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificationbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cours/java/springboot/certificationbackend/CertificationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cours.java.springboot.certificationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
