77646d0052c4f03faaf6a760eebed9fa0ca681a3
abdoul20 <abdoulkarimfoly1996@gmail.com>
2022-06-28 23:20:26 +0000

    creation de la classe dto pour mieu respecter les bonne pratique

---
+++ src/main/java/cours/java/springboot/certificationbackend/dtos/ProfileDTO.java
@@ -0,0 +1,17 @@
+package cours.java.springboot.certificationbackend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import cours.java.springboot.certificationbackend.entities.Utilisateur;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+public class ProfileDTO {
+    private Long id;
+    private String libelle;
+}
